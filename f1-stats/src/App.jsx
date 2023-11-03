1206dd735bce9e2ae127024e695ac7afaa077545
cmbak <chikmii@gmail.com>
2023-11-03 19:38:04 +0000

    Created DriverStandings.jsx

--- f1-stats/src/App.jsx
+++ f1-stats/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import './App.css'
+import DriverStandings from './components/DriverStandings'
 
 function App() {
   return (
     <>
-
+      <DriverStandings />
     </>
   )
 }
