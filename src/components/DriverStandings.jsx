0e3cc389548b4146c8f8233c3f7cc1d3eb1a49dc
cmbak <chikmii@gmail.com>
2023-11-05 16:05:54 +0000

    Added some Todos

--- src/components/DriverStandings.jsx
+++ src/components/DriverStandings.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import LineGraph from './LineGraph'
 
 function DriverStandings() {
+
+    // TODO Get labels for x Axis
+    // TODO Get a list of all the drivers
+
+    // TODO Get data (points in each round in an array) for each driver:
+        //TODO Need to get the number of rounds which have been completed
+        // Then loop from i = 1 to i = completed rounds to get data from each round using `https://ergast.com/api/f1/2023/${i}/driverStandings.json`
+        
+        // IMPORTANT
+        // IF THE DRIVER DIDN'T PARTICIPATE IN THAT ROUND, MAKE SURE POINTS SCORED IN THE ROUND IS 0
+
+        // TODO Convert strings returned from ^ to Integers
+
+        // TODO Find a way of being able to do this for each driver
+
+
     return (
     <>
-        <h1>hi </h1>
-        <LineGraph />
+        <h1>hi</h1>
+        {/* <LineGraph /> */}
     </>
     )
 }
