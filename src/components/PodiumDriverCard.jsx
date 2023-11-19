5eb691e5d7ff12c83aadbfab7e62d29a9c6fbf8c
cmbak <chikmii@gmail.com>
2023-11-19 21:08:51 +0000

    Added styling for podium cards to help visualise layout

--- src/components/PodiumDriverCard.jsx
+++ src/components/PodiumDriverCard.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+// Passed in position of driver which should be displayed
+// Then we get data based on latest round - could this be changed in future so that year can be specified
+
+// Name, driver number (if applicable!)
+// Team
+// Picture
+// Wins
+// Podiums
+// Poles
+// Points
+
 function PodiumDriverCard() {
     return (
         <div className='podium-driver-card'>
@@ -7,11 +18,14 @@ function PodiumDriverCard() {
                 For drivers who are currently 1st, 2nd and 3rd in standings
                 If champion already, set name etc. of driver to gold? - How can this be calculated?
              */}
-            <h1>Lewis Hamilton</h1>
-            <em>#44</em>
-            <h2>Picture</h2>
-            <em>Mercedes AMG Petronas Formula One Team</em>
-            <table>
+            <div className='podium-card-driver-info'>
+                <h1>Lewis Hamilton</h1>
+                <em>#44</em>
+                <h2>Picture</h2>
+                <em>Mercedes AMG Petronas Formula One Team</em>
+                <p>Points: 232</p>
+            </div>
+            <table className='podium-card-stats'>
                 <tbody>
                     <tr>
                         <th>Wins</th>
