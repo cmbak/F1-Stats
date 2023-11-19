564a765fce5269da52b94b3afd863c67e8bccd2b
cmbak <chikmii@gmail.com>
2023-11-19 22:56:57 +0000

    Added template code to get the driver's img (from wikipedia) for PodiumDriverCard

--- src/components/PodiumDriverCard.jsx
+++ src/components/PodiumDriverCard.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { useState } from 'react'
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-// Passed in position of driver which should be displayed
-// Then we get data based on latest round - could this be changed in future so that year can be specified
-
-// Name, driver number (if applicable!)
-// Team
-// Picture
-// Wins
+// driver number (if applicable!)
 // Podiums
 // Poles
-// Points
 
 function PodiumDriverCard({ position, year }) {
-    //const [driverData, setDriverData] = useState({});
     async function retrieveDriverData() {
         const response = await axios.get(`https://ergast.com/api/f1/${year}/driverStandings.json`);
         return response.data;
     }
 
     const { isLoading, isError, data, error} = useQuery('driverData', retrieveDriverData);
+    const WIKI_IMAGE_BASE_URL = "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&formatversion=2&prop=pageimages|pageterms&piprop=original&titles="
 
     if (isLoading) {
         return <span>Loading...</span>
@@ -32,8 +24,27 @@ function PodiumDriverCard({ position, year }) {
     }
 
     const DRIVER_DATA = (data.MRData.StandingsTable.StandingsLists[0].DriverStandings[position-1]) // OR! Can search through DriverStandings and get object w/ position=position? Which would make more sense?
-    const { Driver: { familyName, givenName, permanentNumber }, points, wins } = DRIVER_DATA;
-    
+    const { Driver: { familyName, givenName, permanentNumber, url }, points, wins } = DRIVER_DATA;
+
+    // To get the image we need to know the name of the driver's wikipedia page - found after the last / of url ^
+    const DRIVER_WIKI_PAGE_NAME = url.substring(url.lastIndexOf('/')+1);
+    const DRIVER_WIKI_IMG_URL = WIKI_IMAGE_BASE_URL + DRIVER_WIKI_PAGE_NAME;
+
+    let imgUrl;
+    // limit 200?
+    async function getDriverImg() {
+        const response = await axios.get(DRIVER_WIKI_IMG_URL).then(response => {
+            console.log(response.data.query.pages[0].original.source);
+        }).catch(error => console.log(error));
+    }
+
+    //getDriverImg()
+
+    // TODO - Change getDriverImg to use react-query?
+    // TODO - Change width of img
+    // TODO - Match img sizes from ^
+    // TODO - Style Cards
+
     return (
         <div className='podium-driver-card'>
             {/*
@@ -43,7 +54,7 @@ function PodiumDriverCard({ position, year }) {
             <div className='podium-card-driver-info'>
                 <h1>{`${givenName} ${familyName}`}</h1>
                 <em>#{permanentNumber}</em>
-                <h2>Picture</h2>
+                <img className='podium-driver-img' src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Lewis_Hamilton_2022_S%C3%A3o_Paulo_Grand_Prix_%2852498120773%29_%28cropped%29.jpg" alt={`Picture of ${givenName} ${familyName}`} />
                 <em>{DRIVER_DATA.Constructors[0].name}</em>
                 <p>Points: {points}</p>
             </div>
